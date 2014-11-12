d85652ddaacc16a39cb896ea26183d990bcc5752
isaysthat <isaysthat@comcast.net>
2014-11-12 18:39:48 -0500

    added some documentation

--- primeclient.py
+++ primeclient.py
@@ -1,6 +1,7 @@
 import rpyc
 
-c = rpyc.connect("localhost", 12345)
+c = rpyc.connect("localhost", 12345) #replacing "localhost" with an ip address 
+#will connect to a service at that address
 c.root
 num =int( input('Type a number to check if its prime '))
-print(c.root.is_prime(num))
+print(c.root.is_prime(num)) #calls the service's method is_prime
