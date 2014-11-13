f78330114da689ca0acd28bf99f749cb641d3155
= <isaysthat@comcast.net>
2014-11-12 19:16:06 -0500

    fixed typo

--- primeclient.py
+++ primeclient.py
@@ -2,8 +2,6 @@ import rpyc
 serverAddress = input('Type an ip to connect to ')
 
 c = rpyc.connect(serverAddress, 12345)
-c = rpyc.connect("localhost", 12345) #replacing "localhost" with an ip address 
-#will connect to a service at that address
 c.root
 num =int( input('Type a number to check if its prime '))
 print(c.root.is_prime(num)) #calls the service's method is_prime
