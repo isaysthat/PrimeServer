f5db9e78e05cab8994cabf4531d048d2942c5565
= <isaysthat@comcast.net>
2014-11-12 19:08:58 -0500

    added ip prompt

--- primeclient.py
+++ primeclient.py
@@ -1,6 +1,7 @@
 import rpyc
+serverAddress = input('Type an ip to connect to ')
 
-c = rpyc.connect("localhost", 12345)
+c = rpyc.connect(serverAddress, 12345)
 c.root
 num =int( input('Type a number to check if its prime '))
 print(c.root.is_prime(num))
